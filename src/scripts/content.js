8e28c623026e91bafef45128a4518ad6f7da1bb1
Charles Smart <charlie@Charless-MBP.home>
2019-10-30 09:56:42 -0400

    Add parcel for module bundling

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,19 +1,9 @@
 import '../styles/main.styl';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Screen from '../components/Screen';
 
-(function() {
-
-  appendScreen();
-  freezeBody();
-
-  function freezeBody() {
-    document.body.style.overflow = 'hidden';
-    document.body.style.position = 'fixed';
-  }
-
-  function appendScreen() {
-    let $screen = document.createElement('div');
-    $screen.classList.add('focus__screen');
-    document.body.appendChild($screen);
-  }
-
-})()
+ReactDOM.render(
+  <Screen />,
+  document.body
+);
